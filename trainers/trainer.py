3d9d1ca78ffa061bfa1649c5dd2416aeb2ce91fb
JJCAO <jjcao1231@gmail.com>
2017-09-22 16:26:22 +0800

    for validation

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -131,27 +131,25 @@ class Trainer(object):
                     metrics.tolist() + [''] * 5 + [elapsed_time]
                 log = map(str, log)
                 f.write(','.join(log) + '\n')
-                        
-    def do(self, btrain, data_loader, log_step):
-        self.model.train(btrain)  # Set model to training mode if btrain==True; else to evaluate mode
+    
+    def train_epoch(self, log_step):
+        self.model.train(True)  # Set model to training mode if btrain==True; else to evaluate mode
+        data_loader = self.train_loader
         n_class = len(data_loader.dataset.class_names)
-
-        if btrain==False:
-            val_loss = 0
-            visualizations = []
-            label_trues, label_preds = [], []
         
         for batch_idx, (data, target) in tqdm.tqdm(
                 enumerate(data_loader), total=len(data_loader),
                 desc='Train epoch=%d' % self.epoch, ncols=80, leave=False):
            
             # for resuming
-            if btrain:
-                iter = batch_idx + self.epoch * len(data_loader)
-                if self.iter != 0 and (iter - 1) != self.iter:
-                    continue  
-                self.iter = iter
+            iter = batch_idx + self.epoch * len(data_loader)
+            if self.iter != 0 and (iter - 1) != self.iter:
+                continue  
+            self.iter = iter
 
+            if self.iter % self.interval_validate == 0:
+                self.validate()
+                
             # data preprocessing
             if torch.cuda.is_available():
                 data, target = data.cuda(), target.cuda()
@@ -160,24 +158,43 @@ class Trainer(object):
             # this is not in standard FCN
             #poly_lr_scheduler(self.optim, self.base_l_rate, iter)
              
-            if btrain:# optimize
-                score, loss = self.optimize(data, target)  
-                self.log_csv(score, target, loss, n_class, log_step)  
-                if self.iter >= self.max_iter:
-                    break
-            else:
-                score = self.model(data)
-    
-                loss = cross_entropy2d(score, target, size_average=self.size_average)
-                if np.isnan(float(loss.data[0])):
-                    raise ValueError('loss is nan while validating')
-                val_loss += float(loss.data[0]) / len(data)
-                self.visualize(data, score, target,label_preds, label_trues, visualizations, n_class)
+            # optimize
+            score, loss = self.optimize(data, target)  
+            self.log_csv(score, target, loss, n_class, log_step)  
+            
+            if self.iter >= self.max_iter:
+                break
+        #end of for
+                    
+    def validate(self):
+        data_loader = self.val_loader
+        self.model.train(False)  # Set model to training mode if btrain==True; else to evaluate mode
+        n_class = len(data_loader.dataset.class_names)
+
+        val_loss = 0
+        visualizations = []
+        label_trues, label_preds = [], []
+        
+        for batch_idx, (data, target) in tqdm.tqdm(
+                enumerate(data_loader), total=len(data_loader),
+                desc='Train epoch=%d' % self.epoch, ncols=80, leave=False):        
+
+            # data preprocessing
+            if torch.cuda.is_available():
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            
+            score = self.model(data)
+
+            loss = cross_entropy2d(score, target, size_average=self.size_average)
+            if np.isnan(float(loss.data[0])):
+                raise ValueError('loss is nan while validating')
+            val_loss += float(loss.data[0]) / len(data)
+            self.visualize(data, score, target,label_preds, label_trues, visualizations, n_class)
         #end of for
         
-        # for outputing in validation
-        if btrain==False:    
-            self.output(val_loss, visualizations, label_preds, label_trues, n_class)
+        ###########   
+        self.output(val_loss, visualizations, label_preds, label_trues, n_class)
             
     def visualize(self, data, score, target, label_preds, label_trues, visualizations,n_class):
         imgs = data.data.cpu()
@@ -239,15 +256,13 @@ class Trainer(object):
         
         for epoch in tqdm.trange(self.epoch, max_epoch, desc='Train', ncols=80):
             self.epoch = epoch
+
+            self.train_epoch(log_step)
+            
+            if self.iter >= self.max_iter:
+                break
+            
             if epoch % self.lrDecayEvery == 0:
                 self.l_rate = self.l_rate * self.l_rate_decay
                 for param_group in self.optim.param_groups:
                     param_group['lr'] = self.l_rate
-
-            if self.iter % self.interval_validate == 0:
-                self.do(False, self.val_loader, log_step)
-            
-            self.do(True, self.train_loader, log_step)
-            
-            if self.iter >= self.max_iter:
-                break
