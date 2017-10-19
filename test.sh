9b3921d22664f4c906acae27ce5f5bae99b7e9eb
JJCAO <jjcao1231@gmail.com>
2017-10-19 17:51:57 +0800

    tesh.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-python test.py -m '../../output/fcn32s_model_best_nclass_2.pth.tar' 
-              -i '../../data/foot_data/sc_test/origin' 
-            -o '../../output/tmp' 
-             -g 1
+#
+#              -i '../../data/foot_data/sc_test/origin' \
+python test.py -m '../../output/fcn32s_model_best_nclass_2.pth.tar' -i '../../input'  -o '../../output/tmp' -g 1 
